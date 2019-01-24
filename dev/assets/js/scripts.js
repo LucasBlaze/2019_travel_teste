7bc4b506fadcaad2128d9e840c7e0266a9f1073c
Lucas Primo <luckasprimo@hotmail.com>
2019-01-24 18:13:50 -0200

    menu mobile

--- dev/assets/js/scripts.js
+++ dev/assets/js/scripts.js
@@ -1,20 +1,52 @@
 $(function(){
-  var Main = {
-    init : function(){
-      Main.bind.init.call();
-    },
-    cache : {
-    
-    },
-    bind : {
-      init: function(){
-        
-      },
-    },
-    functions : {
-      
-    }
-  }
+	var Main = {
+		init : function(){
+			Main.bind.init.call();
+		},
+		cache : {
+			menuIcon: $('.s-index-header__menu-icon'),
+			nav: $('.c-nav'),
+			menuItem: $('.c-nav__list-item'),
+			slider: $('.c-slider__container')
+		},
+		bind : {
+			init: function(){
+				
+				Main.cache.menuIcon.click(function(){
+					Main.functions.toggleMenu.call();
+				});
+				
+				Main.cache.menuItem.click(function(){
+					Main.functions.toggleSubMenu.call(this);
+				});
 
-  Main.init.call();
+				Main.functions.initSlider.call();
+
+			},
+		},
+		functions : {
+			toggleMenu: function(){
+				Main.cache.menuIcon.toggleClass('open');
+				Main.cache.nav.toggleClass('open');
+			},
+			toggleSubMenu: function(){
+				$(this).toggleClass('open').next('.c-nav__hidden-list').slideToggle();
+			},
+			initSlider: function(){
+				Main.cache.slider.slick({
+					dots: false,
+					infinite: true,
+					speed: 500,
+					fade: true,
+					cssEase: 'linear',
+					prevArrow: $('.slider__arrow--prev'),
+					nextArrow: $('.slider__arrow--next')
+				});	
+			}
+			
+
+		}
+	}
+
+	Main.init.call();
 });
